01c7a3170fba38e9409e4db6caa34e64336884cd
MaksGovor <maksgovruha@gmail.com>
2021-10-16 15:37:43 +0300

    Add implementation of from, pathsFromStart, pathsToGoal, solution

--- streams/src/main/scala/streams/Solver.scala
+++ streams/src/main/scala/streams/Solver.scala
@@ -36,7 +36,7 @@ trait Solver extends GameDef {
    */
   def newNeighborsOnly(neighbors: LazyList[(Block, List[Move])],
                        explored: Set[Block]): LazyList[(Block, List[Move])] = {
-    for { (block, moves) <- neighbors if !explored.contains(block) } yield (block, moves)
+    for { (block, moves) <- neighbors if !(explored contains block) } yield (block, moves)
   }
 
   /**
@@ -63,18 +63,30 @@ trait Solver extends GameDef {
    * construct the correctly sorted lazy list.
    */
   def from(initial: LazyList[(Block, List[Move])],
-           explored: Set[Block]): LazyList[(Block, List[Move])] = ???
+           explored: Set[Block]): LazyList[(Block, List[Move])] = {
+    if (initial.isEmpty) LazyList.empty
+    else {
+      val more = for {
+        (block, moves) <- initial
+        next <- newNeighborsOnly(neighborsWithHistory(block, moves), explored)
+      } yield next
+      initial #::: from(more, explored ++ more.map(_._1))
+    }
+  }
 
   /**
    * The lazy list of all paths that begin at the starting block.
    */
-  lazy val pathsFromStart: LazyList[(Block, List[Move])] = ???
+  lazy val pathsFromStart: LazyList[(Block, List[Move])] = from(Set((startBlock, Nil)).to(LazyList), Set.empty)
 
   /**
    * Returns a lazy list of all possible pairs of the goal block along
    * with the history how it was reached.
    */
-  lazy val pathsToGoal: LazyList[(Block, List[Move])] = ???
+  lazy val pathsToGoal: LazyList[(Block, List[Move])] = for {
+    (block, moves) <- pathsFromStart
+    if (done(block))
+  } yield (block, moves)
 
   /**
    * The (or one of the) shortest sequence(s) of moves to reach the
@@ -84,5 +96,8 @@ trait Solver extends GameDef {
    * the first move that the player should perform from the starting
    * position.
    */
-  lazy val solution: List[Move] = ???
+  lazy val solution: List[Move] = pathsToGoal match {
+    case (_, moves) #:: xs => moves.reverse
+    case _ => Nil
+  }
 }
